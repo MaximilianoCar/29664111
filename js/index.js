a6d239afe5f9dfe850d9a9b576bfb7d06ff2e984
MaximilianoCar <maximilianoc2002@gmail.com>
2024-05-13 15:51:32 -0400

    perfil con información dinámica

--- js/index.js
+++ js/index.js
@@ -1,12 +1,14 @@
+// Index
 const estudiantesLista = document.getElementById('estudiantes-lista');
 
-// Function to create a list item (li) element for a student
-function createStudentListItem(estudiante) {
+// Funcion para crear un elemento li por estudiante
+function CrearEstudiante(estudiante) {
   const li = document.createElement('li');
   li.classList.add('img');
+  li.dataset.estudianteCi = estudiante.ci; 
 
   const img = document.createElement('img');
-  img.src = `reto5/${estudiante.imagen}`; // Combine path with image name from JSON
+  img.src = `reto5/${estudiante.imagen}`; 
 
   const p = document.createElement('p');
   p.classList.add('text');
@@ -15,21 +17,76 @@ function createStudentListItem(estudiante) {
   li.appendChild(img);
   li.appendChild(p);
 
+  li.addEventListener('click', handleStudentClick);
+
   return li;
 }
 
-// Function to fetch and display student data
-function loadStudentData() {
-  fetch('reto5/datos/index.json') // Replace with your actual JSON file path
+function handleStudentClick(event) {
+    const clickedElement = event.target;
+  
+      const studentCi = clickedElement.dataset.estudianteCi;
+  
+      console.log(studentCi)
+      const studentDetailURL = `perfil.html?ci=${studentCi}`; 
+  
+      window.location.href = studentDetailURL;
+  }
+
+// Funcionn para mostrar datos
+function AgregarEstudiantes() {
+  fetch('reto5/datos/index.json') 
     .then(response => response.json())
     .then(data => {
       data.forEach(estudiante => {
-        const studentListItem = createStudentListItem(estudiante);
+        const studentListItem = CrearEstudiante(estudiante);
         estudiantesLista.appendChild(studentListItem);
       });
     })
     .catch(error => console.error('Error fetching data:', error));
 }
 
-// Load student data on page load
-loadStudentData();
+document.addEventListener('DOMContentLoaded', function() {
+    AgregarEstudiantes();
+  });
+
+  //Perfil
+
+const urlParams = new URLSearchParams(window.location.search);
+const ci = urlParams.get('ci');
+
+const nombre = document.querySelector('.desc');
+
+function AgregarInfoEstudiante() {
+  fetch(`reto5/${ci}/perfil.json`) 
+    .then(response => response.json())
+    .then(estudiante => {
+
+      document.querySelector('.tituloPerfil').textContent = estudiante.nombre;
+
+      const img = document.querySelector('.fotoperfil');
+      img.src = `reto5/${ci}/${estudiante.imagen}`;
+      img.alt = 'Foto de ' + estudiante.nombre;
+
+      document.getElementById('nombre').textContent = estudiante.nombre;
+      document.querySelector('.desc').textContent = estudiante.descripcion;
+      document.querySelector('.color').textContent = estudiante.color;
+      document.querySelector('.libro').textContent = estudiante.libro.join(', ');
+      document.querySelector('.musica').textContent = estudiante.musica.join(', ');
+      document.querySelector('.juegos').textContent = estudiante.video_juego.join(', ');
+
+      const lenguajes = document.querySelector('.lenguajes');
+      const negrita = document.createElement('b');
+      negrita.textContent = estudiante.lenguajes.join(", ");
+      lenguajes.appendChild(negrita);
+
+      document.querySelector('.mail').href = `mailto:${estudiante.email}`;
+      document.querySelector('.mail').textContent = estudiante.email;
+      
+    })
+    .catch(error => console.error('Error fetching data:', error));
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+  AgregarInfoEstudiante();
+});
