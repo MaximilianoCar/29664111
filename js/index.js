314b07e743e7b4a0a30ada8d4ffefe9f63ec28b7
MaximilianoCar <maximilianoc2002@gmail.com>
2024-05-16 22:07:39 -0400

    perfil con información dinámica

--- js/index.js
+++ js/index.js
@@ -1,59 +1,159 @@
-// Index
-const estudiantesLista = document.getElementById('estudiantes-lista');
+let idiomaActual
 
-// Funcion para crear un elemento li por estudiante
-function CrearEstudiante(estudiante) {
-  const li = document.createElement('li');
-  li.classList.add('img');
-  li.dataset.estudianteCi = estudiante.ci; 
+//configuracion de idioma
 
-  const img = document.createElement('img');
-  img.src = `reto5/${estudiante.imagen}`; 
+// Establecer idioma actual
 
-  const p = document.createElement('p');
-  p.classList.add('text');
-  p.textContent = estudiante.nombre;
+const urlParams0 = new URLSearchParams(window.location.search);
+idiomaActual = urlParams0.get('lang') || 'ES'; // Idioma por defecto: "ES"
 
-  li.appendChild(img);
-  li.appendChild(p);
+function ActualizarConf(configuracionActual){
+  // Actualizar título de la página
+  document.title = configuracionActual.sitio.join(" ");
 
-  li.addEventListener('click', handleStudentClick);
+  // Actualizar elementos del header
+  document.querySelector('.titulo').textContent = configuracionActual.sitio[0];
+  const titulo = document.querySelector('.titulo');
+  const span = document.createElement('span');
+  span.textContent = configuracionActual.sitio[1];
 
-  return li;
+  const textNode = document.createTextNode(configuracionActual.sitio[2]);
+  titulo.appendChild(span);
+  titulo.appendChild(textNode);
+
+  document.querySelector('.nombres').textContent = configuracionActual.saludo;
+  document.querySelector('.buscador').placeholder = configuracionActual.buscar;
+  document.querySelector('.boton').value = configuracionActual.buscar;
+
+  // Actualizar contenido del footer
+  document.querySelector('footer p').textContent = configuracionActual.copyRight;
+
 }
 
-function handleStudentClick(event) {
-    const clickedElement = event.target;
-  
-      const studentCi = clickedElement.dataset.estudianteCi;
-  
-      console.log(studentCi)
-      const studentDetailURL = `perfil.html?ci=${studentCi}`; 
-  
-      window.location.href = studentDetailURL;
+function ActualizarConfPerfil(configuracionActual){
+  // Actualizar título de la página
+  document.title = configuracionActual.sitio.join(" ");
+  document.getElementById('color').textContent = configuracionActual.color;
+  document.getElementById('libro').textContent = configuracionActual.libro;
+  document.getElementById('musica').textContent = configuracionActual.musica;
+  document.getElementById('juegos').textContent = configuracionActual.video_juego;
+  document.getElementById('lenguajes').textContent = configuracionActual.lenguajes;
+  document.querySelector('.correo').textContent = configuracionActual.email;
+
+}
+  // Index
+  const estudiantesLista = document.getElementById('estudiantes-lista');
+
+  // Funcion para crear un elemento li por estudiante
+  function CrearEstudiante(estudiante) {
+    const li = document.createElement('li');
+    li.classList.add('img');
+    li.dataset.estudianteCi = estudiante.ci; 
+
+    const img = document.createElement('img');
+    img.src = `reto5/${estudiante.imagen}`; 
+
+    const p = document.createElement('p');
+    p.classList.add('text');
+    p.textContent = estudiante.nombre;
+
+    li.appendChild(img);
+    li.appendChild(p);
+
+    li.addEventListener('click', handleStudentClick);
+
+    return li;
   }
 
-// Funcionn para mostrar datos
-function AgregarEstudiantes() {
-  fetch('reto5/datos/index.json') 
-    .then(response => response.json())
-    .then(data => {
-      data.forEach(estudiante => {
-        const studentListItem = CrearEstudiante(estudiante);
-        estudiantesLista.appendChild(studentListItem);
-      });
-    })
-    .catch(error => console.error('Error fetching data:', error));
-}
+  function handleStudentClick(event) {
+      const clickedElement = event.target;
+    
+        const studentCi = clickedElement.dataset.estudianteCi;
+    
+        console.log(studentCi)
+        const studentDetailURL = `perfil.html?ci=${studentCi}&lang=${idiomaActual}`; 
+    
+        window.location.href = studentDetailURL;
+    }
 
-document.addEventListener('DOMContentLoaded', function() {
-    AgregarEstudiantes();
-  });
+  // Funcionn para mostrar datos
+  function AgregarEstudiantes() {
+    fetch('reto5/datos/index.json') 
+      .then(response => response.json())
+      .then(data => {
+        data.forEach(estudiante => {
+          const studentListItem = CrearEstudiante(estudiante);
+          estudiantesLista.appendChild(studentListItem);
+        });
+
+        //configuracion de idioma
+
+        // Establecer idioma actual para index
+
+        if (idiomaActual === "ES") {
+          fetch('reto5/conf/configES.json') 
+            .then(response => response.json())
+            .then(configuracionActual => {
+              ActualizarConf(configuracionActual);
+            })
+
+        } else if (idiomaActual === "EN") {
+          fetch('reto5/conf/configEN.json') 
+            .then(response => response.json())
+            .then(configuracionActual => {
+              ActualizarConf(configuracionActual);
+            })
+
+        } else if(idiomaActual === "PT"){
+          fetch('reto5/conf/configPT.json') 
+            .then(response => response.json())
+            .then(configuracionActual => {
+              ActualizarConf(configuracionActual);
+            })
+        }else{
+          console.error("Idioma no encontrado:", idiomaActual);
+        }
+            
+        //buscador
+        const searchInput = document.querySelector('.menuForm');
+
+        searchInput.addEventListener('keyup', handleSearch);
+
+        function handleSearch(event) {
+          const searchTerm = event.target.value.toLowerCase().trim();
+        
+          const filteredStudents = data.filter(estudiante => estudiante.nombre.toLowerCase().includes(searchTerm));
+        
+          estudiantesLista.innerHTML = '';
+
+          if(filteredStudents.length > 0){
+            filteredStudents.forEach(estudiante => {
+              const studentListItem = CrearEstudiante(estudiante);
+              estudiantesLista.appendChild(studentListItem);
+            });
+          } else {
+            let message = document.createElement('li');
+            message.classList.add('noResults');
+            message.textContent = configuracionActual.sinResultado ;
+            list.appendChild(message);
+          }
+        
+        }
+
+      })
+      .catch(error => console.error('Error fetching data:', error));
+  }
+
+  document.addEventListener('DOMContentLoaded', function() {
+      AgregarEstudiantes();
+    });
+
 
   //Perfil
 
 const urlParams = new URLSearchParams(window.location.search);
 const ci = urlParams.get('ci');
+idiomaActual = urlParams.get('lang') || 'ES'; // Idioma por defecto: "ES"
 
 const nombre = document.querySelector('.desc');
 
@@ -62,6 +162,30 @@ function AgregarInfoEstudiante() {
     .then(response => response.json())
     .then(estudiante => {
 
+      if (idiomaActual === "ES") {
+        fetch('reto5/conf/configES.json') 
+          .then(response => response.json())
+          .then(configuracionActual => {
+            ActualizarConfPerfil(configuracionActual);
+          })
+      
+      } else if (idiomaActual === "EN") {
+        fetch('reto5/conf/configEN.json') 
+          .then(response => response.json())
+          .then(configuracionActual => {
+            ActualizarConfPerfil(configuracionActual);
+          })
+      
+      } else if(idiomaActual === "PT"){
+        fetch('reto5/conf/configPT.json') 
+          .then(response => response.json())
+          .then(configuracionActual => {
+            ActualizarConfPerfil(configuracionActual);
+          })
+      }else{
+        console.error("Idioma no encontrado:", idiomaActual);
+      }
+      
       document.querySelector('.tituloPerfil').textContent = estudiante.nombre;
 
       const img = document.querySelector('.fotoperfil');
@@ -87,6 +211,7 @@ function AgregarInfoEstudiante() {
     .catch(error => console.error('Error fetching data:', error));
 }
 
-document.addEventListener('DOMContentLoaded', function() {
+  document.addEventListener('DOMContentLoaded', function() {
   AgregarInfoEstudiante();
 });
+
